50d70c66626f7458e12df4d0591d9d27cffca852
Narely Najera <nnarely@yahoo.com>
2019-01-30 16:48:43 -0500

    testing piepline with mvc challenge [163606199]

--- DemoProject/DemoProject.Domain/Program.cs
+++ DemoProject/DemoProject.Domain/Program.cs
@@ -6,7 +6,7 @@ namespace DemoProject.Domain
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! NArely Njera");
         }
     }
 }
