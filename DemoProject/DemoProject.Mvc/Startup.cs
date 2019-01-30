bf1cd66b6300c4e65cb57ca3a847c1115b683557
Narely Najera <nnarely@yahoo.com>
2019-01-30 16:47:45 -0500

    testing pipeline with mvc challenge

--- DemoProject/DemoProject.Mvc/Startup.cs
+++ DemoProject/DemoProject.Mvc/Startup.cs
@@ -14,10 +14,14 @@ namespace DemoProject.Mvc
 {
     public class Startup
     {
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
+
+
+
 
         public IConfiguration Configuration { get; }
 
